872ef6e2498f099e77202ece238e9f47de329e80
lajumbo <1050517806@qq.com>
2021-11-16 09:37:22 +0800

    pytest例子
    Signed-off-by: lajumbo <1050517806@qq.com>

---
+++ test/test_sample.py
@@ -0,0 +1,14 @@
+# content of test_sample.py
+import pytest
+
+
+def inc(x):
+    return x + 1
+
+
+def test_answer():
+    assert inc(3) == 5
+
+
+if __name__ == '__main__':
+    pytest.main(['test_sample.py'])
