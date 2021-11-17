674b56cff0d585f56c757fa967e59a7cfa7a12f0
lajumbo <1050517806@qq.com>
2021-11-17 11:17:40 +0800

    计算器测试demo
    Signed-off-by: lajumbo <1050517806@qq.com>

---
+++ test/mycode/calculator.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+# 被测代码，计算器
+class Calculator:
+    def add(self, a, b):
+        return a + b
+
+    def sub(self, a, b):
+        return a - b
+
+    def mul(self, a, b):
+        return a * b
+
+    def div(self, a, b):
+        return a / b
